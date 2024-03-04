4f2d3e629cd92975c37259d124b7eba0c5a9162c
bishalbanstola <bishalbanstola10@gmail.com>
2024-03-04 00:13:37 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// app.js
+
+async function initAuth0() {
+  const auth0 = await createAuth0Client({
+    domain: "dev-xvv0pvrqkgxsikp3.us.auth0.com",
+    client_id: "Dj9BctQtEdC47bvIrMVQJc2H0kypuc6X",
+    redirect_uri:
+      "file:///C:/Users/benba/OneDrive/Desktop/wddm/web design/web-design-project-api/html/index.html",
+  });
+
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  const loginButton = document.getElementById("login");
+  loginButton.addEventListener("click", async () => {
+    await auth0.loginWithRedirect();
+  });
+
+  const logoutButton = document.getElementById("logout");
+  logoutButton.addEventListener("click", async () => {
+    await auth0.logout({
+      returnTo: window.location.origin,
+    });
+  });
+
+  const profileElement = document.getElementById("profile");
+  const updateProfile = async () => {
+    const user = await auth0.getUser();
+    profileElement.innerText = JSON.stringify(user, null, 2);
+  };
+
+  if (isAuthenticated) {
+    profileElement.style.display = "block";
+    updateProfile();
+  } else {
+    profileElement.style.display = "none";
+  }
+}
+
+initAuth0();
