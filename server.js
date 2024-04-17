9146f2d44598494f102550ba49702ae85f619e4c
bishal <bishalbanstola10@gmail.com>
2024-04-17 17:32:27 -0400

    added env variable

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
@@ -12,10 +13,22 @@ const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
 require('express-async-errors');
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+//const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+        type: "service_account",
+        project_id: process.env.PROJECT_ID,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        private_key:process.env.PRIVATE_KEY,
+        client_email: process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        auth_uri: process.env.AUTH_URI,
+        token_uri: process.env.TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+        universe_domain: process.env.UNIVERSE_DOMAIN
+    }),
     storageBucket: 'gs://project-web-design-bb61f.appspot.com'
 });
 
